28999631892094957215d2228f7cb5f915e1dfcb
GaganNarula <gagan@earthspecies.org>
2025-02-05 09:11:00 +0100

    Adding project level config constants

---
+++ esp_data/config/project_config.py
@@ -0,0 +1,14 @@
+REQUIRED_DATASAMPLE_FIELDS = [
+    "id",
+    "source_dataset",
+    "metadata",
+]
+
+
+REQUIRED_DATASET_FIELDS = [
+    "name",
+    "creator",
+    "version",
+    "description",
+    "sources",
+]
