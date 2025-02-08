0f07faa16d8d157b1a9a6189d97c1603ad130693
GaganNarula <gagan@earthspecies.org>
2025-02-08 21:07:17 +0100

    First run of beans0 complete

--- scripts/prepare_beans0.py
+++ scripts/prepare_beans0.py
@@ -2,7 +2,10 @@
 
 import argparse
 import json
+import logging
 import os
+import pdb
+import pickle
 from typing import Optional
 
 import yaml
@@ -16,11 +19,28 @@ from esp_data.config import DataSample, DatasetConfig
 from esp_data.dataset import HFDataset
 from esp_data.file_io import GSAudioFile
 
+# load .env from project root
 load_dotenv()
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+console.setFormatter(formatter)
+logger.addHandler(console)
+
+file_handler = logging.FileHandler("prepare_beans0.log")
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 
 CREATOR = "M.Hagiwara; masato at earthspecies dot org"
 STORAGE_OPTIONS = {"project": os.getenv("GCP_DEFAULT_PROJECT")}
+LOCAL_CACHE_DIR = "./temp_cache"
 
 DATASET_JSONL_PATHS = {
     "esc50": "gs://foundation-model-data/data/esc50-animal/test_processed.jsonl",
@@ -31,6 +51,8 @@ DATASET_JSONL_PATHS = {
     "hiceas": "gs://foundation-model-data/data/hiceas-detection/test_processed.jsonl",
     "rfcx": "gs://foundation-model-data/data/rfcx-detection/test_processed.jsonl",
     "gibbons": "gs://foundation-model-data/data/hainan-gibbons-detection/test_processed.jsonl",
+    "lifestage": "gs://foundation-model-data/data/animalspeak-lifestage/v1.32/test_filtered_processed_license.jsonl",
+    "call-type": "gs://foundation-model-data/data/animalspeak-call-type/v1.32/test_classification_subset_processed.jsonl",
 }
 
 LICENSES = {
@@ -42,6 +64,8 @@ LICENSES = {
     "hiceas": "data are free to use without restriction",
     "watkins": "free for personal/academic uses",
     "gibbons": "CC-BY-NC-SA",  # rest are missing or are at individual file level
+    "lifestage": "per file licenses, please see individual files",
+    "call-type": "per file licenses, please see individual files",
 }
 
 ALL_DATASET_NAMES = list(DATASET_JSONL_PATHS.keys())
@@ -57,7 +81,11 @@ def load_beans_metadata():
 
     metadata = {b["name"]: b for b in beans_cfg if b["name"] in DATASET_JSONL_PATHS}
     # HACK, have to add gibbons separately because of naming inconsistency
-    metadata["gibbons"] = [b for b in beans_cfg if "gibbons" in b["name"]]
+    metadata["gibbons"] = [b for b in beans_cfg if "gibbons" in b["name"]][0]
+    # HACK, check with Masato and David
+    # for lifestage and call-type, there are several possible datasets in beans that match
+    metadata["lifestage"] = [b for b in beans_cfg if "lifestage" in b["name"]]
+    metadata["call-type"] = [b for b in beans_cfg if "call-type" in b["name"]]
 
     return metadata
 
@@ -72,13 +100,13 @@ def read_jsonl_from_bucket(file_path: str | os.PathLike) -> list[dict]:
             return json.load(f)["annotation"]
 
         except Exception as e:
-            print(f"Error reading jsonl {e}, trying line by line")
+            logger.error(f"Error reading jsonl {e}, trying line by line")
             # read lines
             records = f.readlines()
             return [json.loads(record) for record in records]
 
         except Exception as e:
-            print(f"Error reading jsonl {e}")
+            logger.error(f"Error reading jsonl {e}")
             return []
 
 
@@ -104,11 +132,20 @@ class BeansSample(DataSample):
     label: Optional[str] = Field(default=None, description="Optional label for the audio")
 
 
-def make_sample(row: dict, source_dataset: str, license: str, replace_16k: bool = True) -> dict:
+def make_sample(
+    row: dict, source_dataset: str, license: str, replace_16k: bool = True, audio_filenames: set = ()
+) -> dict:
     """Make a single BeansSample from a row in a component Beans0 dataset."""
 
     path_parts = AnyPath(row["path"]).parts
+
+    # check if the audio filename already exists
+    if os.path.basename(path_parts[-1]) in audio_filenames:
+        logger.error(f"Audio filename {path_parts[-1]} already exists")
+        return None
+
     ## VERY HACKY, issue is paths are local to David's VM
+    pdb.set_trace()
     idx_go = path_parts.index("foundation-model-data")
     # ASSUMPTION: all paths are in the form gs://foundation-model-data/...
     path = "gs://" + "/".join(path_parts[idx_go:])
@@ -120,20 +157,28 @@ def make_sample(row: dict, source_dataset: str, license: str, replace_16k: bool 
     audio_file = GSAudioFile(path)
 
     if not audio_file.exists:
-        print(f"Sample not found at {path}")
+        logger.error(f"Sample not found at {path}")
         return None
 
     try:
-        audio, sr = audio_file.read_audio()
+        audio, _ = audio_file.read_audio()
     except Exception as e:
-        print(f"Error reading audio {e}")
+        logger.error(f"Error reading audio {e}")
         return None
+
+    # check if row has a license and recordist field
+    metadata = {}
+    if "license" in row:
+        license = row["license"]
+    if "recordist" in row:
+        recordist = row["recordist"]
+        metadata = {"recordist": recordist}
 
     try:
         sample = BeansSample(
             source_dataset=source_dataset,
             license=license,
-            metadata={},
+            metadata=metadata,
             path=os.path.basename(path),
             audio=list(audio),
             text=row.get("text", None),
@@ -143,12 +188,17 @@ def make_sample(row: dict, source_dataset: str, license: str, replace_16k: bool 
         return sample.to_dict()
 
     except Exception as e:
-        print(f"Pydantic Validation error {e}")
+        logger.error(f"Pydantic Validation error {e}")
         return None
 
 
 def make_component_dataset(
-    dataset_cfg, output_dir: str | os.PathLike, batch_size: int = 100, replace_16k: bool = True, state: dict = None
+    dataset_cfg,
+    output_dir: str | os.PathLike | AnyPath,
+    batch_size: int = 100,
+    replace_16k: bool = True,
+    state: dict = None,
+    use_local_cache: bool = False,
 ):
     """Create a Huggingface dataset from a component Beans0 dataset.
 
@@ -161,12 +211,14 @@ def make_component_dataset(
     """
     name = dataset_cfg["name"]
     # read the jsonl
-    rows = read_jsonl_from_bucket(dataset_cfg["path"], fs)
-    print(f"#### PROCESSING DATASET {name} with {len(rows)} samples ####\n")
+    rows = read_jsonl_from_bucket(dataset_cfg["path"])
+    logger.info(f"#### PROCESSING DATASET {name} with {len(rows)} samples ####\n")
 
     batch = []
     not_found = []
+    audio_filenames = set()
     shard_idx = 0
+    ds = None
     config = DatasetConfig(
         name=name,
         creator=CREATOR,
@@ -183,6 +235,7 @@ def make_component_dataset(
             "shard_idx": shard_idx,
             "idx": 0,
             "config": config,
+            "audio_filenames": audio_filenames,
         }
 
     for idx, row in tqdm(enumerate(rows, start=state["idx"]), total=len(rows) - state["idx"]):
@@ -193,6 +246,7 @@ def make_component_dataset(
             source_dataset=name,
             license=dataset_cfg["license"],
             replace_16k=replace_16k,
+            audio_filenames=state["audio_filenames"],
         )
 
         if sample is None:
@@ -200,30 +254,57 @@ def make_component_dataset(
             continue
 
         state["batch"].append(sample)
+        state["audio_filenames"].add(sample["path"])
 
         if len(batch) == batch_size:
-            try:
+            if use_local_cache:
                 ds = HFDataset.from_samples(batch, config)
-
+                ds.save_to_path(path=AnyPath(LOCAL_CACHE_DIR) / f"{name}/shard_{shard_idx}", save_config=False)
+                ds.save_config(AnyPath(LOCAL_CACHE_DIR) / f"{name}")
+            else:
+                ds = HFDataset.from_samples(batch, config)
                 # do not save config, we can add it later
                 ds.save_to_path(
                     path=AnyPath(output_dir) / f"shard_{shard_idx}", storage_options=STORAGE_OPTIONS, save_config=False
                 )
-
-            except Exception as e:
-                print(e)
 
             shard_idx += 1
             batch = []
             state["batch"] = batch
             state["shard_idx"] = shard_idx
 
-        # save state, over-write local file
-        with open("state.json", "w") as f:
-            state["config"] = config.to_dict(make_serializable=True)
-            json.dump(state, f)
+        # save state as pickle
+        if idx % 100 == 0:
+            with open("state.pkl", "wb") as f:
+                pickle.dump(state, f)
 
-    return state
+    # save the last batch
+    if len(batch) > 0:
+        if use_local_cache and len(rows) > batch_size and ds is not None:
+            ds = HFDataset.from_samples(batch, config)
+            ds.save_to_path(path=AnyPath(LOCAL_CACHE_DIR) / f"{name}/shard_{shard_idx}", save_config=False)
+
+        elif ds is None:
+            # only one shard smaller than batch size
+            ds = HFDataset.from_samples(batch, config)
+            ds.save_to_path(path=AnyPath(output_dir), storage_options=STORAGE_OPTIONS, save_config=False)
+
+        else:
+            ds = HFDataset.from_samples(batch, config)
+            ds.save_to_path(
+                path=AnyPath(output_dir) / f"shard_{shard_idx}", storage_options=STORAGE_OPTIONS, save_config=False
+            )
+
+    ds.save_config(output_dir)
+
+    if use_local_cache and len(rows) > batch_size:
+        # concatenate all the shards
+        ds = HFDataset.from_path(AnyPath(LOCAL_CACHE_DIR) / f"{name}", sharded=True)
+        ds.save_to_path(path=AnyPath(output_dir), storage_options=STORAGE_OPTIONS, save_config=False)
+        # remove the temp cache
+        # shutil.rmtree(AnyPath(LOCAL_CACHE_DIR) / f"{name}")
+
+    return state, ds
 
 
 def main():
@@ -233,6 +314,11 @@ def main():
         type=str,
         default="gs://foundation-model-data/data/beans0",
         help="The directory to save the dataset to",
+    )
+    parser.add_argument(
+        "--use_local_cache",
+        action="store_true",
+        help="Whether to use local cache. This will save batches on disk locally before concatenating and uploading to GCS",
     )
     parser.add_argument(
         "--components_to_process",
@@ -267,8 +353,8 @@ def main():
     args = parser.parse_args()
 
     if args.resume_from_state:
-        with open("state.json", "r") as f:
-            state = json.load(f)
+        with open("state.pkl", "rb") as f:
+            state = pickle.load(f)
             state["config"] = DatasetConfig(**state["config"])
     else:
         state = None
@@ -278,25 +364,35 @@ def main():
         if name not in ALL_DATASET_NAMES:
             raise ValueError(f"Invalid dataset name {name}")
 
-    for name in args.components_to_process:
+    if len(args.versions) == 1:
+        args.versions = args.versions * len(args.components_to_process)
+
+    if len(args.components_to_process) != len(args.versions):
+        raise ValueError("Number of components to process and versions must be the same")
+
+    for n, name in enumerate(args.components_to_process):
         dataset_cfg = {
             "name": name,
             "path": DATASET_JSONL_PATHS[name],
             "license": LICENSES.get(name, "unknown"),
-            "version": args.versions[ALL_DATASET_NAMES.index(name)],
+            "version": args.versions[n],
             "description": f"""This is a component dataset of the BEANS0 eval dataset.
             It has following metadata:\n\n
             {json.dumps(METADATA[name], indent=4)}
             """,
         }
 
-        make_component_dataset(
+        output_dir = AnyPath(args.output_dir) / f"{name}"
+        _, ds = make_component_dataset(
             dataset_cfg,
-            args.output_dir,
+            output_dir,
             args.batch_size,
             args.replace_16k,
             state,
+            args.use_local_cache,
         )
+
+        logger.info(f"Finished processing {name} with {len(ds)} samples")
 
 
 if __name__ == "__main__":
