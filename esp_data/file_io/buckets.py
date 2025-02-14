f82554407cfd9b521c99db84700d95d6519f7581
GaganNarula <gagan@earthspecies.org>
2025-02-14 19:12:15 +0100

    FileSystem based bucket classes

--- esp_data/file_io/buckets.py
+++ esp_data/file_io/buckets.py
@@ -8,13 +8,16 @@ import subprocess
 import warnings
 from typing import Generator
 
-from cloudpathlib import AnyPath, CloudPath
-from gcsfs import GCSFileSystem
+from cloudpathlib import CloudPath
+from dotenv import load_dotenv
 from google.cloud.storage import Client as GSClient
 from google.cloud.storage import transfer_manager
 
-from ..utils import is_cloud_path, is_local_path
+from esp_data.paths import AnyPath, is_cloud_path, is_local_path, strip_cloud_prefix
 
+from .utils import make_cloudflarer2fs, make_fs, make_gcsfs
+
+load_dotenv()
 # TODO: This is a temporary fix to suppress the warning about using end user credentials
 # see here: https://github.com/googleapis/google-auth-library-python/issues/271 for info
 warnings.filterwarnings("ignore", "Your application has authenticated using end user credentials")
@@ -45,7 +48,7 @@ class Bucket:
     def exists(self):
         return self.bucket_path.exists()
 
-    def list_files(self, recursive: bool = True) -> list[CloudPath]:
+    def list_files(self, recursive: bool = True, as_str: bool = False) -> list[CloudPath]:
         if recursive:
             iterator = self.bucket_path.rglob("*")
         else:
@@ -53,15 +56,15 @@ class Bucket:
             # though it is not a file
             iterator = self.bucket_path.glob("*")
 
-        return [file for file in iterator if file.is_file()]
+        return [str(file) if as_str else file for file in iterator if file.is_file()]
 
-    def list_dirs(self, recursive: bool = True) -> list[CloudPath]:
+    def list_dirs(self, recursive: bool = True, as_str: bool = False) -> list[CloudPath]:
         if recursive:
             iterator = self.bucket_path.rglob("*")
         else:
             iterator = self.bucket_path.iterdir()
 
-        return [dir for dir in iterator if dir.is_dir()]
+        return [str(dir) if as_str else dir for dir in iterator if dir.is_dir()]
 
     def subdir_as_bucket(self, subdir: str | os.PathLike | AnyPath) -> "Bucket":
         """Create a subdirectory as a Bucket object.
@@ -70,13 +73,10 @@ class Bucket:
         gs://esp-ci-cd-tests/esp-data-tests/subdir.
         """
         subpath = self.bucket_path / AnyPath(subdir)
-        if not subpath.exists():
-            raise FileNotFoundError(f"Subpath {subpath} does not exist.")
 
         return Bucket(subpath)
 
     def find_paths_with_extension(self, extension: str) -> Generator[CloudPath, None, None]:
-        assert extension.startswith("."), "Extension must start with a period e.g. .wav"
         for file in self.bucket_path.rglob(f"*{extension}"):
             yield CloudPath(file)
 
@@ -370,12 +370,186 @@ class GSBucket(Bucket):
             other._rsync(self, delete_unmatched, continue_on_error, recursive, gzip_in_flight)
 
 
-# TODO: Implement a version of GSBucket that uses the GCSFileSystem from gcsfs
-class GSBucketV2(GSBucket):
-    """This version of Google cloud storage uses the GCSFileSystem from gcsfs.
-    It is more efficient than cloudpathlib and google-cloud-storage.
+class FSBucket:
+    """A Bucket that uses an underlying filesystem class.
+
+    Args:
+
+        bucket_path: The path to the bucket.
+        fs: The filesystem object to use for the bucket.
+
     """
 
-    def __init__(self, bucket_path):
-        super().__init__(bucket_path)
-        self.fs = GCSFileSystem()
+    def __init__(self, bucket_path: str | os.PathLike | AnyPath, fs=None):
+        if fs is None:
+            fs = make_fs(bucket_path)
+
+        bucket_path = strip_cloud_prefix(str(bucket_path))  # remove any cloud prefix, fsspec doesn't like it
+        self.bucket_path = AnyPath(bucket_path)  # not using CloudPath because it doesn't work with r2 paths
+        self.fs = fs
+
+    def _exists(self, path: str | os.PathLike | AnyPath) -> bool:
+        return self.fs.exists(str(path))
+
+    @property
+    def exists(self):
+        return self.fs.exists(str(self.bucket_path))
+
+    def fopen(self, file_path: str | os.PathLike, mode: str = "rb", **fopen_kwargs):
+        """Open a file in the bucket."""
+        file_path = AnyPath(file_path)
+
+        return self.fs.open(str(file_path), mode, **fopen_kwargs)
+
+    def list_dirs(self, recursive: bool = True) -> list[AnyPath]:
+        if recursive:
+            search_str = "**"
+        else:
+            search_str = "*"
+
+        return [AnyPath(dir) for dir in self.fs.glob(str(self.bucket_path / search_str)) if self.fs.isdir(dir)]
+
+    def list_files(self, recursive: bool = True) -> list[AnyPath]:
+        if recursive:
+            search_str = "**"
+        else:
+            search_str = "*"
+
+        return [AnyPath(file) for file in self.fs.glob(str(self.bucket_path / search_str)) if self.fs.isfile(file)]
+
+    def find_files_with_extension(self, extension: str) -> list[AnyPath]:
+        return [
+            AnyPath(file) for file in self.fs.glob(str(self.bucket_path / f"**/*{extension}")) if self.fs.isfile(file)
+        ]
+
+    def find_files_containing(self, substring: str) -> list[AnyPath]:
+        return [
+            AnyPath(file) for file in self.fs.glob(str(self.bucket_path / f"**/*{substring}*")) if self.fs.isfile(file)
+        ]
+
+    def subdir_as_bucket(self, subdir: str | os.PathLike | AnyPath) -> "FSBucket":
+        """Create a subdirectory as a Bucket object.
+        e.g. if the bucket path is gs://esp-ci-cd-tests/esp-data-tests/,
+        and subdir is "subdir", then the new bucket path will be
+        gs://esp-ci-cd-tests/esp-data-tests/subdir.
+        """
+        subpath = self.bucket_path / AnyPath(subdir)
+
+        return FSBucket(subpath, self.fs)  # I'm not sure if this is the right way to do this, using the same fs ?
+
+    def upload_dir(self, source_dir: str | os.PathLike | AnyPath, destination: str | os.PathLike | AnyPath) -> None:
+        """Upload a local directory to a destination folder in this bucket."""
+        if not is_local_path(source_dir):
+            raise ValueError("Source dir must be a local path.")
+
+        try:
+            self.fs.put(str(source_dir), str(self.bucket_path / AnyPath(destination)), recursive=True)
+        except Exception as e:
+            raise RuntimeError(f"Error uploading {source_dir} to {destination}: {e}")
+        logger.info(f"Uploaded {source_dir} to {destination}")
+
+    def download_to(self, destination: str | os.PathLike) -> None:
+        """Download the bucket contents to a local path. Does recursive download."""
+        if not is_local_path(destination):
+            raise ValueError("Destination path must be a local path.")
+
+        try:
+            self.fs.get(str(self.bucket_path), str(destination), recursive=True)
+        except Exception as e:
+            raise RuntimeError(f"Error downloading to {destination}: {e}")
+
+        logger.info(f"Downloaded to {destination}")
+
+    def move_dir(self, source: str | os.PathLike, destination: str | os.PathLike, confirm: bool = False) -> None:
+        """Move a source folder in this bucket to a destination folder within the same bucket.
+        CAUTION: This will overwrite files in the destination if they have the same name as files in the source.
+        AND it will delete the source folder after moving.
+
+        Args:
+            source: The source folder to move.
+            destination: The destination folder.
+            confirm: Whether to confirm before moving. Default is False
+
+        Raises:
+            FileNotFoundError: If the source path does not exist.
+        """
+        source = self.bucket_path / AnyPath(source)
+        destination = self.bucket_path / AnyPath(destination)
+
+        if confirm:
+            if not input(f"Are you sure you want to move {source} to {destination}? (y/n): ").lower() == "y":
+                return
+
+        try:
+            self.fs.mv(str(source), str(destination))
+            logger.info(f"Moved {source} to {destination}")
+        except Exception as e:
+            raise RuntimeError(f"Error moving {source} to {destination}: {e}")
+
+    def delete_dir(self, dir_path: str | os.PathLike, confirm: bool = True):
+        """Delete a directory in the bucket. When = "", this means the whole bucket!"""
+        if is_cloud_path(dir_path):
+            raise ValueError("dir path must be relative to the bucket.")
+        dir_path = self.bucket_path / AnyPath(dir_path) / "**"
+
+        if confirm:
+            if not input(f"Are you sure you want to delete {dir_path}? (y/n): ").lower() == "y":
+                return
+
+        try:
+            # first find all files in the dir and delete them
+            self.fs.rm(str(dir_path), recursive=True)
+            logger.info(f"Deleted dir {dir_path} from {self.bucket_path}")
+
+        except Exception as e:
+            raise RuntimeError(f"Error deleting {dir_path}: {e}")
+
+    async def async_upload_dir(self, source_dir: str | os.PathLike, destination: str | os.PathLike) -> None:
+        loop = asyncio.get_running_loop()
+        with concurrent.futures.ThreadPoolExecutor() as pool:
+            await loop.run_in_executor(pool, self.upload_dir, source_dir, destination)
+
+    async def async_download_to(self, destination: str | os.PathLike) -> None:
+        loop = asyncio.get_running_loop()
+        with concurrent.futures.ThreadPoolExecutor() as pool:
+            await loop.run_in_executor(pool, self.download_to, destination)
+
+    async def async_move_dir(
+        self, source: str | os.PathLike, destination: str | os.PathLike, confirm: bool = False
+    ) -> None:
+        loop = asyncio.get_running_loop()
+        with concurrent.futures.ThreadPoolExecutor() as pool:
+            await loop.run_in_executor(pool, self.move_dir, source, destination, confirm)
+
+    def __repr__(self):
+        return f"R2Bucket({self.bucket_path})"
+
+    def __str__(self):
+        return str(self.bucket_path)
+
+
+class GSBucketV2(FSBucket):
+    """This version of Google cloud storage uses the GCSFileSystem from gcsfs.
+    It seems more efficient than cloudpathlib and google-cloud-storage.
+
+    Args:
+        bucket_path: The path to the bucket. This may either be the bucket name
+        or a path like gs://bucket_name/some_folder.
+    """
+
+    def __init__(self, bucket_path: str | os.PathLike | AnyPath):
+        self.bucket_path = AnyPath(strip_cloud_prefix(str(bucket_path)))
+        self.fs = make_gcsfs()
+
+
+class R2Bucket(FSBucket):
+    """A Cloudflare R2 bucket that uses the aws s3 api (s3fs filesystem).
+
+    Args:
+        bucket_path: The path to the bucket. This may either be the bucket name
+        or a path like r2://bucket_name/some_folder.
+    """
+
+    def __init__(self, bucket_path: str | os.PathLike | AnyPath):
+        self.bucket_path = AnyPath(strip_cloud_prefix(str(bucket_path)))
+        self.fs = make_cloudflarer2fs()
