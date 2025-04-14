bddc8d50bb67c3f17902c0fd157cba51795f2d98
CheekySparrow <cheeky@sparrow.com>
2025-04-14 10:07:41 +0100

    read_utils

--- esp_data/io/__init__.py
+++ esp_data/io/__init__.py
@@ -1,5 +1,5 @@
 from esp_data.io.functional import get_fs
-from esp_data.io.parsers import read_audio
 from esp_data.io.paths import AnyPath, GSPath, R2Path
+from esp_data.io.read_utils import read_audio
 
-__all__ = ["AnyPath", "GSPath", "R2Path", "read_audio"]
+__all__ = ["AnyPath", "GSPath", "R2Path", "read_audio", "get_fs"]
