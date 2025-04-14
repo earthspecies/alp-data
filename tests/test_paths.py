cbcb66e81e6c1570368491e3ed9fda33617d90e3
CheekySparrow <cheeky@sparrow.com>
2025-04-14 17:06:14 +0100

    quickly cleaning

--- tests/test_paths.py
+++ tests/test_paths.py
@@ -13,7 +13,6 @@ def test_local_path():
     assert path.is_local
     assert path.exists()
 
-
 @pytest.mark.parametrize(
     "cloud_path",
     [
@@ -21,9 +20,26 @@ def test_local_path():
         "r2://esp-ci-cd-tests/esp-data-tests/file1.txt",
     ],
 )
-def test_cloud_path(cloud_path):
+@pytest.mark.skip(reason="No upload for automated CI.")
+def test_cloud_upload_path(cloud_path):
     path = anypath(cloud_path)
     path.upload_from("tests/fileio_test_folder/file1.txt")
+    assert not path.is_local
+    assert path.is_cloud
+    assert path.exists()
+    assert path.is_file()
+    assert path.read_bytes() == b"hello\n"
+    path.unlink()
+    assert not path.exists()
+
+@pytest.mark.parametrize(
+    "cloud_path",
+    [
+        "gs://esp-ci-cd-tests/esp-data-tests/file1.txt",
+    ],
+)
+def test_cloud_read_path(cloud_path):
+    path = anypath(cloud_path)
     assert not path.is_local
     assert path.is_cloud
     assert path.exists()
