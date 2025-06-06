47f38af20afbfb7ad0e353c91f386ed20cefe2c1
Gagan Narula <gagan@earthspecies.org>
2025-06-06 12:10:35 +0200

    New datasets (#44)
    New datasets:
    
    + InsectSet459
    + Beans
    + BarkleyCanyonDetection (this needs to be debugged though)
    
    + tests and datasets documentation.
    + `data_root` argument added to the datasets which allows a user to
    change the location of a dataset (e.g. if the dataset is moved from the
    bucket to a disk). This should be replaced with a `from_disk` method in
    v2.2.
    
    Have not yet fixed the docstring issues in transforms.

--- esp_data/datasets/__init__.py
+++ esp_data/datasets/__init__.py
@@ -1,7 +1,12 @@
 from .animalspeak import AnimalSpeak
-from .barkley_canyon import BarkleyCanyon
+from .barkley_canyon import BarkleyCanyon, BarkleyCanyonDetection
+from .insectset_459 import InsectSet459
+from .beans import Beans
 
 __all__ = [
     "AnimalSpeak",
     "BarkleyCanyon",
+    "BarkleyCanyonDetection",
+    "InsectSet459",
+    "Beans",
 ]
