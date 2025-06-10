1b5894f1ca8f60f0d809ce7c5db1599aa8a21a70
Name <miron.marius@gmail.com>
2025-06-10 16:05:53 +0200

    added birdset wip

--- esp_data/datasets/__init__.py
+++ esp_data/datasets/__init__.py
@@ -2,6 +2,7 @@ from .animalspeak import AnimalSpeak
 from .barkley_canyon import BarkleyCanyon, BarkleyCanyonDetection
 from .insectset_459 import InsectSet459
 from .beans import Beans
+from .birdset import BirdSet
 
 __all__ = [
     "AnimalSpeak",
@@ -9,4 +10,5 @@ __all__ = [
     "BarkleyCanyonDetection",
     "InsectSet459",
     "Beans",
+    "BirdSet",
 ]
