e0a05194a6831c8cc479886228a390fb7f32e962
David Robinson <66266528+david-rx@users.noreply.github.com>
2025-06-19 22:59:56 +1000

    add giant otters (#67)
    Giant otters
    
    Co-authored-by: David <david@earthspecies.org>

--- esp_data/datasets/__init__.py
+++ esp_data/datasets/__init__.py
@@ -2,12 +2,14 @@ from .animalspeak import AnimalSpeak
 from .barkley_canyon import BarkleyCanyon, BarkleyCanyonDetection
 from .beans import Beans
 from .birdset import BirdSet
+from .giant_otters import GiantOtters
 from .insectset_459 import InsectSet459
 
 __all__ = [
     "AnimalSpeak",
     "BarkleyCanyon",
     "BarkleyCanyonDetection",
+    "GiantOtters",
     "InsectSet459",
     "Beans",
     "BirdSet",
