b9337a54ed0a4d68ea37faf78383be498572c8d4
GaganNarula <gagan@earthspecies.org>
2025-07-07 20:53:45 +0200

    Refactoring and adding xeno_canto scripts

---
+++ scripts/data_preprocessing_scripts/xeno_canto_resample.py
@@ -0,0 +1,71 @@
+"""Resample new xeno-canto files and copy them to target"""
+
+import argparse
+
+import librosa
+import pandas as pd
+import soundfile as sf
+
+from esp_data.io import anypath, read_audio
+
+
+def main() -> None:
+    """Resample new xeno-canto files and copy them to target directory."""
+
+    parser = argparse.ArgumentParser(
+        description="Resample new xeno-canto files and copy them to target directory."
+    )
+    parser.add_argument(
+        "--filepaths_to_copy",
+        type=str,
+        required=True,
+        help="Path to the CSV file containing file paths to copy.",
+    )
+    parser.add_argument(
+        "--target_dir",
+        type=str,
+        required=True,
+        help="Target directory to copy the resampled files to.",
+    )
+    parser.add_argument(
+        "--target_sr",
+        type=int,
+        default=16000,
+        help="Sample rate to which the audio files should be resampled.",
+    )
+
+    args = parser.parse_args()
+    target_dir = anypath(args.target_dir)
+
+    # Read the CSV file containing file paths to copy
+    df = pd.read_csv(args.filepaths_to_copy)
+    df = df.dropna(subset=["files"]).tolist()
+
+    for i, file_path in enumerate(df):
+        print(f"Processing file number {i}/{len(df)}: {file_path}")
+
+        # Read the audio file
+        data, sr = read_audio(file_path)
+
+        # Resample if necessary
+        if sr != args.target_sr:
+            data = librosa.resample(
+                y=data,
+                orig_sr=sr,
+                target_sr=args.target_sr,
+                scale=True,
+                res_type="kaiser_best",
+            )
+
+        # Define the target path
+        target_path = target_dir / (anypath(file_path).stem + ".flac")
+
+        # Write the resampled audio as flac file
+        with target_path.open("wb") as f:
+            sf.write(f, data, args.target_sr, format="FLAC")
+
+        print(f"Copied and resampled {file_path} to {target_path}")
+
+
+if __name__ == "__main__":
+    main()
