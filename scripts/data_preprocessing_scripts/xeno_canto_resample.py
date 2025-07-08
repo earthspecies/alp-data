81813e34e8f0c6d29569ad2f8ceb0f0cfd3e720f
GaganNarula <gagan@earthspecies.org>
2025-07-08 09:24:23 +0200

    Error handling

--- scripts/data_preprocessing_scripts/xeno_canto_resample.py
+++ scripts/data_preprocessing_scripts/xeno_canto_resample.py
@@ -40,14 +40,28 @@ def main() -> None:
     # Read the CSV file containing file paths to copy
     df = pd.read_csv(args.filepaths_to_copy)
     df = df.dropna(subset=["files"])
+    errors = []
 
     print(f"Found {len(df)} files to process.")
     for i, row in df.iterrows():
         file_path = row["files"]
+
+        # Define the target path
+        target_path = target_dir / (anypath(file_path).stem + ".flac")
+
+        if target_path.exists():
+            print(f"Skipping {file_path}, already exists at {target_path}")
+            continue
+
         print(f"Processing file number {i}/{len(df)}: {file_path}")
 
         # Read the audio file
-        data, sr = read_audio(file_path)
+        try:
+            data, sr = read_audio(file_path)
+        except Exception as e:
+            print(f"Error reading {file_path}: {e}")
+            errors.append((file_path, str(e)))
+            continue
 
         data = audio_stereo_to_mono(data, mono_method="average").squeeze()
 
@@ -61,14 +75,20 @@ def main() -> None:
                 res_type="kaiser_best",
             )
 
-        # Define the target path
-        target_path = target_dir / (anypath(file_path).stem + ".flac")
-
         # Write the resampled audio as flac file
         with target_path.open("wb") as f:
             sf.write(f, data, args.target_sr, format="FLAC")
 
         print(f"Copied and resampled {file_path} to {target_path}")
+
+    # Save errors to a CSV file
+    if errors:
+        error_df = pd.DataFrame(errors, columns=["file_path", "error"])
+        error_file = target_dir / "resample_and_copy_errors.csv"
+        error_df.to_csv(error_file, index=False)
+        print(f"Errors encountered during processing saved to {error_file}")
+    else:
+        print("All files processed successfully with no errors.")
 
 
 if __name__ == "__main__":
