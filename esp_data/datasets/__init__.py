66ed33e078792472ddde99799c3d7914183d62e0
Benjamin Hoffman <benjaminsshoffman@gmail.com>
2025-10-30 18:03:15 +0000

    add hawaiian birds

--- esp_data/datasets/__init__.py
+++ esp_data/datasets/__init__.py
@@ -7,6 +7,7 @@ from .birdset import BirdSet
 from .chiffchaff_id import ChiffchaffId
 from .esp_raincoast import ESPRaincoast
 from .giant_otters import GiantOtters
+from .hawaiian_birds import HawaiianBirds
 from .insectset_459 import InsectSet459
 from .littleowl_id import LittleOwlId
 from .macaques_coo_calls import MacaquesCooCalls
@@ -34,4 +35,5 @@ __all__ = [
     "VoxaboxenEvents",
     "ESPRaincoast",
     "WABAD",
+    "HawaiianBirds",
 ]
