0c152b82af4d53bbc94ef6bff908376db1e3b9bb
Benjamin Hoffman <72874445+benjaminsshoffman@users.noreply.github.com>
2025-12-11 04:42:45 -0900

    update wabad dataset (#186)
    WABAD was the first dataset I implemented, and as a result it was the
    lowest quality implementation. I wanted to updated it since WABAD is
    probably the most important dataset we have for detection evals.
    
    Here, I've updated the dataset so that it is more self-contained (i.e.
    no weird label mapping logic), and also had the full suite of tests we
    want to run (e.g. hashing for the underlying data).

--- esp_data/datasets/subsegmentation.py
+++ esp_data/datasets/subsegmentation.py
@@ -165,7 +165,7 @@ class Subsegmentation(Dataset):
                 scale=True,
                 res_type="kaiser_best",
             )
-        sr = target_sr
+            sr = target_sr
 
         # Selection table
         st = pd.read_csv(StringIO(row["selection_table"]), sep="\t")
