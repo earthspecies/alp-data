4e26d9b247c17615e69c3b20ff206cea1d8b8bb7
Benjamin Hoffman <benjaminsshoffman@gmail.com>
2026-03-17 18:20:03 +0000

    add ebird conversions

--- esp_data/discover/__init__.py
+++ esp_data/discover/__init__.py
@@ -1,3 +1,19 @@
+from .ebird_taxonomy import (
+    ClementsToEBird,
+    ClementsToEBirdConfig,
+    EBirdConverter,
+    EBirdToClements,
+    EBirdToClementsConfig,
+)
 from .gbif_taxonomy import AddTaxonomy, AddTaxonomyConfig, GBIFConverter
 
-__all__ = ["GBIFConverter", "AddTaxonomy", "AddTaxonomyConfig"]
+__all__ = [
+    "GBIFConverter",
+    "AddTaxonomy",
+    "AddTaxonomyConfig",
+    "EBirdConverter",
+    "EBirdToClements",
+    "EBirdToClementsConfig",
+    "ClementsToEBird",
+    "ClementsToEBirdConfig",
+]
