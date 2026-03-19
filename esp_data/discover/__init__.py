cc78e3f0d5e45ea9417f67e6bd57429b5441be05
Benjamin Hoffman <benjaminsshoffman@gmail.com>
2026-03-19 18:43:06 +0000

    Revert "add ebird conversions"
    This reverts commit 4e26d9b247c17615e69c3b20ff206cea1d8b8bb7.

--- esp_data/discover/__init__.py
+++ esp_data/discover/__init__.py
@@ -1,19 +1,3 @@
-from .ebird_taxonomy import (
-    ClementsToEBird,
-    ClementsToEBirdConfig,
-    EBirdConverter,
-    EBirdToClements,
-    EBirdToClementsConfig,
-)
 from .gbif_taxonomy import AddTaxonomy, AddTaxonomyConfig, GBIFConverter
 
-__all__ = [
-    "GBIFConverter",
-    "AddTaxonomy",
-    "AddTaxonomyConfig",
-    "EBirdConverter",
-    "EBirdToClements",
-    "EBirdToClementsConfig",
-    "ClementsToEBird",
-    "ClementsToEBirdConfig",
-]
+__all__ = ["GBIFConverter", "AddTaxonomy", "AddTaxonomyConfig"]
