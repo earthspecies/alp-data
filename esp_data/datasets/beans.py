b4ee83e2e0610fa1fed11cf146bdd0596698ae18
Gagan Narula <gagan@earthspecies.org>
2026-05-05 16:37:35 +0200

    INaturalist and Beans fixes (#280)
    - Convert inaturalist m4a files to wav (our `read_audio` method cant
    handle m4a right now, but we might want to support it going forward
    because there were more than 80,000 m4a files in inaturalist alone, or
    we convert during data ingestion).
    -  Also needed to convert broken inaturalist mp3 files -> wav
    - fix split audio paths in inat
    - fix esc50 paths in train/val/test splits of beans (this should be the
    last change)
    - Some scripts used for clean up and audio conversion

--- esp_data/datasets/beans.py
+++ esp_data/datasets/beans.py
@@ -46,9 +46,9 @@ class Beans(Dataset):
         name="beans",
         owner="gagan",
         split_paths={
-            "train": "gs://esp-ml-datasets/beans/v0.1.0/raw/beans_train_v2.csv",
-            "validation": "gs://esp-ml-datasets/beans/v0.1.0/raw/beans_val_v2.csv",
-            "test": "gs://esp-ml-datasets/beans/v0.1.0/raw/beans_test_v2.csv",
+            "train": "gs://esp-ml-datasets/beans/v0.1.0/raw/beans_train_v3.csv",
+            "validation": "gs://esp-ml-datasets/beans/v0.1.0/raw/beans_val_v3.csv",
+            "test": "gs://esp-ml-datasets/beans/v0.1.0/raw/beans_test_v3.csv",
             "cbi_test": "gs://esp-ml-datasets/beans/v0.1.0/raw/cbi_test.jsonl",
             "cbi_validation": "gs://esp-ml-datasets/beans/v0.1.0/raw/cbi_val.jsonl",
             "cbi_train": "gs://esp-ml-datasets/beans/v0.1.0/raw/cbi_train.jsonl",
